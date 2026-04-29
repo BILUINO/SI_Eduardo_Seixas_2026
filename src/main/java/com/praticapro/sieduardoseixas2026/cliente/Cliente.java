21517a9c0282825a00e41b9517dec151af3aed5b
Eduardo <eduardoseixas367@gmail.com>
2026-04-28 21:40:53 -0300

    teste

--- src/main/java/com/praticapro/sieduardoseixas2026/cliente/Cliente.java
+++ src/main/java/com/praticapro/sieduardoseixas2026/cliente/Cliente.java
@@ -19,7 +19,7 @@ public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id; //oi
 
     @Column(nullable = false, length = 100)
     private String nome;
